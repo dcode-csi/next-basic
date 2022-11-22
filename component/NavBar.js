4e57106b0471c0080c7ff306775dffa9cceb40dd
tubi55 <tubi55@nate.com>
2022-11-22 19:22:15 +0900

    style jsx를 활용한 스타일링

--- component/NavBar.js
+++ component/NavBar.js
@@ -1,20 +1,29 @@
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-import styles from './NavBar.module.css';
 
 function NavBar() {
 	const router = useRouter();
 
 	return (
 		<nav>
-			<Link href='/' className={`${styles.link} ${router.pathname === '/' ? styles.active : ''}`}>
-				Home
+			{/* NextJS 13버전업되면서 Link안에 a를 넣을수 없게 바뀌었으나 그러면 style jsx가 안 먹음 : legacyBehavior적용 */}
+			<Link href='/' legacyBehavior>
+				<a className={router.pathname === '/' ? 'active' : ''}>Home</a>
 			</Link>
-			<Link
-				href='/about'
-				className={`${styles.link} ${router.pathname === '/about' ? styles.active : ''}`}>
-				About
+			<Link href='/about' legacyBehavior>
+				<a className={router.pathname === '/about' ? 'active' : ''}>About</a>
 			</Link>
+			<style jsx>{`
+				nav {
+					background-color: tomato;
+				}
+				a {
+					text-decoration: none;
+				}
+				.active {
+					color: yellow;
+				}
+			`}</style>
 		</nav>
 	);
 }
