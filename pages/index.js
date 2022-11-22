1282ebd88cf2bc1415b75876d10c2605b9bfbe5e
tubi55 <tubi55@nate.com>
2022-11-22 20:01:21 +0900

    Head 컴포넌트로 분리

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,10 @@
-import Head from 'next/head';
+import Seo from '../component/Seo';
 
 export default function Home() {
 	return (
 		<div>
-			<Head>
-				<title>Home | Next Movies</title>
-			</Head>
+			{/* 이런식으로  중괄호 없이 바로 prop도 전달가능 */}
+			<Seo title='Home' />
 			<h1>Hello</h1>
 		</div>
 	);
