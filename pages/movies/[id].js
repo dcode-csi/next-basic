813ec0286749edfe788a62d60cf51754a11f3dda
tubi55 <tubi55@nate.com>
2022-11-23 18:49:00 +0900

    상세페이지에서 새로고침해서 router로 받은 정보값이 없으면 메인페이지 강제 이동

--- pages/movies/[id].js
+++ pages/movies/[id].js
@@ -1,12 +1,17 @@
 import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 
 export default function Detail() {
 	const router = useRouter();
-	console.log(router);
+	//console.log(router);
+
+	useEffect(() => {
+		if (!router.query.title) router.push('/');
+	}, []);
 
 	return (
 		<div>
-			<h4>{router.query.title || 'Loading...'}</h4>
+			<h4>{router.query.title}</h4>
 		</div>
 	);
 }
