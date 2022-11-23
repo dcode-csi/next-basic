8df4f9b184b49d0973190e539488c65b7c14c415
tubi55 <tubi55@nate.com>
2022-11-23 10:19:34 +0900

    영화 포스터 이미지 호출 및 스타일링

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import { useEffect, useState } from 'react';
 import Seo from '../component/Seo';
 
@@ -17,14 +18,44 @@ export default function Home() {
 	}, []);
 
 	return (
-		<div>
+		<div className='container'>
 			<Seo title='Home' />
 			{!Movies && <h4>Loading...</h4>}
 			{Movies?.map((movie) => (
-				<div key={movie.id}>
-					<h4>{movie.original_title}</h4>
+				<div key={movie.id} className='movie'>
+					<img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} />
+					<h4>
+						<Link href={`/movies/${movie.original_title}/${movie.id}`} legacyBehavior>
+							<a>{movie.original_title}</a>
+						</Link>
+					</h4>
 				</div>
 			))}
+
+			<style jsx>{`
+				.container {
+					display: grid;
+					grid-template-columns: 1fr 1fr;
+					padding: 20px;
+					gap: 20px;
+				}
+				.movie {
+					cursor: pointer;
+				}
+				.movie img {
+					max-width: 100%;
+					border-radius: 12px;
+					transition: transform 0.2s ease-in-out;
+					box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
+				}
+				.movie:hover img {
+					transform: scale(1.05) translateY(-10px);
+				}
+				.movie h4 {
+					font-size: 18px;
+					text-align: center;
+				}
+			`}</style>
 		</div>
 	);
 }
