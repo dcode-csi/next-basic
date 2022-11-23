f2b02f143184b22066db914df27bf7d70062b8f1
tubi55 <tubi55@nate.com>
2022-11-23 10:10:29 +0900

    movie api로 부터 영화 데이터 출력

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,30 @@
+import { useEffect, useState } from 'react';
 import Seo from '../component/Seo';
 
+const API_KEY = '4cf85a609f260b43cf0278ad12483b46';
+
 export default function Home() {
+	const [Movies, setMovies] = useState();
+
+	const fetchMovies = async () => {
+		const response = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`);
+		const json = await response.json();
+		setMovies(json.results);
+	};
+
+	useEffect(() => {
+		fetchMovies();
+	}, []);
+
 	return (
 		<div>
-			{/* 이런식으로  중괄호 없이 바로 prop도 전달가능 */}
 			<Seo title='Home' />
-			<h1>Hello</h1>
+			{!Movies && <h4>Loading...</h4>}
+			{Movies?.map((movie) => (
+				<div key={movie.id}>
+					<h4>{movie.original_title}</h4>
+				</div>
+			))}
 		</div>
 	);
 }
